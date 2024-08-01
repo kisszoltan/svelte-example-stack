bcc19e767fca568715f17e36f9ddfb7de2675456
kisszoltan <kiss.zoltan.040016@gmail.com>
2024-08-01 14:00:40 +0200

    Install TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		postcss: {
+			plugins: [tailwindcss(), autoprefixer()]
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
